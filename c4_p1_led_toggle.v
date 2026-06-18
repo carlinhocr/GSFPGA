b972528a392159c17837f9ff0910e20a6c01bc6e
carlinhocr <osolabs@osolabs.tech>
2026-06-18 13:25:46 -0300

    cp4 p1 LED Toggle

--- c4_p1_led_toggle.v
+++ c4_p1_led_toggle.v
@@ -1,20 +1,18 @@
-module Led_Project{
+module Led_Project(
     input i_Clk,
     input i_Switch_1,
     output o_LED_1);
 
-    reg r_LED_1 =1'b0; #led flip flop
-    reg r_Switch_1 =1'b0; #button register output flip flop
+    reg r_LED_1 =1'b0; 
+    reg r_Switch_1 =1'b0; 
 
     always @(posedge i_Clk) 
     begin
         r_Switch_1 <= i_Switch_1;
         if (i_Switch_1 == 1'b0 && r_Switch_1 == 1'b1)
         begin
-            r_LED_1 <= ˜r_LED_1;
+            r_LED_1 <= ~r_LED_1;
         end    
     end
-    assign o_LED_1 <= r_LED_1;
-
+    assign o_LED_1 = r_LED_1;
 endmodule
-}
---
+++ c4_p1_leg_toggle/synlog.tcl
@@ -0,0 +1,1 @@
+project -load c4_p1_leg_toggle_syn.prj
